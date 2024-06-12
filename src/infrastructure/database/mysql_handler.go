ab2a2a3d0cd793cf48adf35bb837a0ad42c0ed4f
JeffWu <itmrchow@gmail.com>
2024-06-12 10:50:18 +0800

    feat: add db display log setting

--- src/infrastructure/database/mysql_handler.go
+++ src/infrastructure/database/mysql_handler.go
@@ -6,6 +6,7 @@ import (
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 
 	"itmrchow/go-project/user/src/domain"
 )
@@ -28,9 +29,17 @@ func NewSqlHandler() DB_Handler {
 		panic("read config error: " + err.Error())
 	}
 
-	formatStr := "%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := fmt.Sprintf(formatStr, config.User, config.Password, config.Host, config.Port, config.Name)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsnStr := "%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf(dsnStr, config.User, config.Password, config.Host, config.Port, config.Name)
+
+	isShowLog := viper.GetBool("mysql.isShowLog")
+
+	db, err := gorm.Open(
+		mysql.Open(dsn),
+		&gorm.Config{
+			Logger: getLogger(isShowLog),
+		},
+	)
 
 	if err != nil {
 		panic("gorm connection error: " + err.Error())
@@ -44,4 +53,12 @@ func NewSqlHandler() DB_Handler {
 	handler.DB = db
 
 	return *handler
+}
+
+func getLogger(isShowLog bool) logger.Interface {
+	if isShowLog {
+		return logger.Default.LogMode(logger.Info)
+	} else {
+		return logger.Default.LogMode(logger.Silent)
+	}
 }

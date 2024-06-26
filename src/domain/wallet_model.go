989a0feeaff8d4fefd73a3d287da4747a7a46986
Jeff Wu <itmrchow@gmail.com>
2024-06-26 17:16:17 +0800

    feat: wallet model add uniqueIndex

--- src/domain/wallet_model.go
+++ src/domain/wallet_model.go
@@ -1,25 +1,10 @@
 package domain
 
 type Wallet struct {
-	UserId     string     `json:"userId" gorm:"type:varbinary(40)"`
-	WalletType WalletType `json:"WalletType" gorm:"varbinary(20)"`
-	Currency   Currency   `json:"currency" gorm:"type:varbinary(10)"` // 幣別
-	Balance    float64    `json:"balance" `                           // 餘額
+	UserId     string  `json:"userId" gorm:"uniqueIndex:uq_userid_type; type:varbinary(40)"`
+	WalletType string  `json:"WalletType" gorm:"uniqueIndex:uq_userid_type; type:varbinary(20)"`
+	Currency   string  `json:"currency" gorm:"type:varbinary(10)"` // 幣別
+	Balance    float64 `json:"balance"`                            // 餘額
 
 	DefaultModel `gorm:"embedded"`
 }
-
-type WalletType string
-
-const (
-	PLATFORM WalletType = "P"
-)
-
-type Currency string
-
-const (
-	PHP  Currency = "PHP"
-	USD  Currency = ""
-	BTC  Currency = "BTC"
-	USDT Currency = "Currency"
-)
